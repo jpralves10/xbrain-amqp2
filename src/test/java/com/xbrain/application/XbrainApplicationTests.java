3b1b634e03e4e6cc7e12709843a5c1f2dd34f432
Jean Alves <jpralves10@gmail.com>
2020-08-13 14:04:15 -0300

    first commit

---
+++ src/test/java/com/xbrain/application/XbrainApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xbrain.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XbrainApplicationTests {
+
+	@Test
+	public void contextLoads() {}
+}
